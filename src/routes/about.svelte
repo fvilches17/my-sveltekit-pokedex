c36947448e070c32c497857a53eb21a5bb452aa1
frankie <frankie@email.com>
2021-10-22 23:46:26 +1300

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About the Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a Svelte Kit demo that builds a pokedex</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {pokemon} from "../stores/pokestore";
+</script>
+
+<svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Svelte Kit Pokedex</h1>
+
+{#each $pokemon as pokeman}
+    <p>{pokeman.name}</p>
+{/each}
