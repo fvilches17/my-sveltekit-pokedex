c4a25a5f92f26512f84cfb3bb7133febb6386bcd
frankie <frankie@email.com>
2021-10-24 13:32:41 +1300

    add pokemon filter func

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script>
 	import { pokemon } from '../stores/pokestore';
 	import PokemanCard from '../components/PokemanCard.svelte';
+
+	let searchTerm;
+	let filteredPokemon = [];
+
+	$: {
+		searchTerm
+			? (filteredPokemon = [...$pokemon.filter((pokeman) => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()))])
+			: (filteredPokemon = [...$pokemon]);
+	}
 </script>
 
 <svelte:head>
@@ -8,9 +17,17 @@
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+<small class="font-mono">Count: {filteredPokemon.length}</small>
+
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	placeholder="Search Pokemon"
+	bind:value={searchTerm}
+/>
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each $pokemon as pokeman}
+	{#each filteredPokemon as pokeman}
 		<PokemanCard {pokeman} />
 	{/each}
 </div>
