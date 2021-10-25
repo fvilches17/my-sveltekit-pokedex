7e1cef8e523a0414f39d15b2523733165973ae00
frankie <frankie@email.com>
2021-10-25 21:29:37 +1300

    add api tester page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -4,7 +4,8 @@
 	let links = [
 		{ id: 1, title: 'Home', path: '/' },
 		{ id: 2, title: 'About', path: '/about' },
-		{ id: 3, title: 'Environment', path: '/environment' }
+		{ id: 3, title: 'Environment', path: '/environment' },
+		{ id: 4, title: 'Api Tester', path: '/apitester' }
 	];
 </script>
 
---
+++ src/routes/apitester.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Header from '$lib/Header.svelte';
+	const title = 'Pokedex Environment Variables';
+
+	let displayGreeting = '...';
+	const loadGreetingManual = async () => {
+		const res = await fetch('api/greetings');
+		const { greeting } = await res.json();
+		displayGreeting = greeting;
+	};
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<Header {title} />
+
+<div class="text-center">
+	<p>{displayGreeting}</p>
+	<button class="bg-gray-900 text-white rounded-md p-2 mt-2" on:click={loadGreetingManual}>
+		Get Greeting
+	</button>
+</div>
